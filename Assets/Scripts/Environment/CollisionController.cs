df9cddcf71a3cc06bf9861e09b67566f143047e6
Gil Morhaim <gil.morhaim@herolo.co.il>
2023-08-26 20:29:12 +0300

    Add camera shaking effect and crash sound on player collision.

--- Assets/Scripts/Environment/CollisionController.cs
+++ Assets/Scripts/Environment/CollisionController.cs
@@ -5,15 +5,29 @@ namespace Environment
 {
     public class CollisionController : MonoBehaviour
     {
-        private const string CollisionAnimationName = "Stumble Backwards";
-        
+        private const string CollisionAnimation = "Stumble Backwards";
+
+        [Header("Game Objects")]
         [SerializeField] private GameObject _player;
         [SerializeField] private GameObject _characterModel;
+        [SerializeField] private GameObject _camera;
+        
+        [Header("Audio Sources")]
+        [SerializeField] private AudioSource _crashSound;
+
+        
         
         private void OnTriggerEnter(Collider other)
         {
             StopPlayerMovement();
-            PlayPlayerCollisionAnimation();
+            PlayThePlayerCollisionAnimation();
+            PlayCrashSoundEffect();
+            PlayShakingCameraAnimation();
+        }
+
+        private void PlayCrashSoundEffect()
+        {
+            _crashSound.Play();
         }
 
         private void StopPlayerMovement()
@@ -22,10 +36,15 @@ namespace Environment
             _player.GetComponent<PlayerMoveController>().enabled = false;
         }
 
-        private void PlayPlayerCollisionAnimation()
+        private void PlayThePlayerCollisionAnimation()
         {
             Animator playerAnimator = _characterModel.GetComponent<Animator>();
-            playerAnimator.Play(CollisionAnimationName);
+            playerAnimator.Play(CollisionAnimation);
+        }
+        
+        private void PlayShakingCameraAnimation()
+        { 
+            _camera.GetComponent<Animator>().enabled = true;
         }
     }
 }
