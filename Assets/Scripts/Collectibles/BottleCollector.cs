ac375992ca35f4eaa422b93381cde47b870b3852
Tomersh97 <tomersh97@gmail.com>
2023-08-27 21:21:46 +0300

    Attached bottle pickup to score display

--- Assets/Scripts/Collectibles/BottleCollector.cs
+++ Assets/Scripts/Collectibles/BottleCollector.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class BottleCollector : MonoBehaviour
 {
+    private const short SINGLE_BOTTLE_SCORE = 1;
     [SerializeField] AudioSource bottleOpenFX;
     private void OnTriggerEnter(Collider other) {
         bottleOpenFX.Play();
+        CollectiblesController.addToScore(SINGLE_BOTTLE_SCORE);
         this.gameObject.SetActive(false);
     }
 }
