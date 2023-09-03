8a0cdf72086ef7ec711bed3c02d39da5b9bb552c
Tomersh97 <tomersh97@gmail.com>
2023-09-03 18:14:05 +0300

    Removed logging for testing

--- Assets/Scripts/Environment/SectionDestroyer.cs
+++ Assets/Scripts/Environment/SectionDestroyer.cs
@@ -7,7 +7,7 @@ public class SectionDestroyer : MonoBehaviour
     [SerializeField] Transform player;
     void Update()
     {
-        Debug.Log("Player Z: " + player.position.z + " " + transform.name + " z: " + transform.position.z);
+        //Debug.Log("Player Z: " + player.position.z + " " + transform.name + " z: " + transform.position.z);
         if (player.position.z - transform.position.z > 50 && transform.name == "GeneratedSection") {
             Destroy(gameObject);
         }
